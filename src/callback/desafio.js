566f51372794c2aec279ea20c0239b4cb2eafc61
Gatotorres <sr.nahueltorres@gmail.com>
2023-10-22 23:53:37 +0200

    sigo sin terminar, solo es otro commit a esta clase de callback

--- src/callback/desafio.js
+++ src/callback/desafio.js
@@ -21,12 +21,28 @@ function fetchData(urlApi,callback){
         // mi código con la API, utilizaremos 'GET' luego se envia la urlApi,
         // si es asincrono es true.
         xhttp.onreadystatechange = function(event){
-        if(xhttp.readystate === 4){
-        if (xhttp.status === 200){
+        //en particular .onreadystatechange tiene un ciclo de vida
+        //de 5 estados de un llamado xmlhtpprequest. el 0,1,2,3 y 4.
+        if(xhttp.readystate === 4){ //*"NÚMEROS MÁGICOS" porque estan sin referencia
+        //cuando el onreadystatechange cambia, 
+        // le preguntamos si el estado es exactamente 4
+        if (xhttp.status === 200){ //*"NÚMEROS MÁGICOS" porque estan sin referencia
+        //entonces solo si el estado es 4, le volvemos a preguntar
+        //si el estado del status es exactamente 200, lo que significaría
+        //que fue recibida, acetpada, procesada correctamente
             callback(null, JSON.parse(xhttp.responseText));
+        //ahora le decimos que al texto que recíbamos, lo pasemos/transformemos
+        //en un JSON. Poreso el JSON.parse. y no se porque el primer
+        //argumento de callback es null
         }}else{
+        //ahora manejaremos el error en caso de recibir uno
             const error = new Error('Error' + urlApi);
+        //esta linea nos devolverá una forma de manejar el error
+        //poniendola dentro de un constructor de objetos new, seguido de ERROR
+        //entre los parentesise le pedimos el link del error para poder leerlo
             return callback(error,null);
+        //
         }
     xhttp.send();
+    //el método .send() envia la petición al servidor
 }
